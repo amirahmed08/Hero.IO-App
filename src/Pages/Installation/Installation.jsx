54c954fc98862a1b4be5eae8ae5191a0c51029f7
unknown <amirahmed7208@gmail.com>
2026-01-21 16:54:47 +0600

    third commit

--- src/Pages/Installation/Installation.jsx
+++ src/Pages/Installation/Installation.jsx
@@ -10,14 +10,27 @@ const Installation = () => {
     const storedAppData = getInstalledApp();
     const convertedStoredApp = storedAppData.map(id=>parseInt(id))
     const installedApp =data.filter(app => convertedStoredApp.includes(app.id))
-    set
+    setAppList(installedApp)
   },[])
   return (
     <div className='bg-base-200'>
       <div className='max-w-[1200px] mx-auto'>
-        {
+         <div>
+          <div className='p-8'>
+            <h1 className='text-center text-4xl font-bold'>Your Installed Apps</h1>
+            <p className='text-center text-gray-500 font-semibold mt-4'>Explore All Trending Apps on the Market developed by us</p>
+          </div>
+          <div className='flex justify-between items-center w-full mb-5'>
+            <h2 className='text-2xl font-bold '>1.Apps Found</h2>
+            <button className='btn btn-ghost'>Sort by Size</button>
+          </div>
+         
+        </div>
+        <div className='space-y-4 mb-10'>
+          {
           appList.map(b => <InstalledApp key={b.id} b={b}></InstalledApp>)
         }
+        </div>
       </div>
     </div>
   )
--- src/Pages/InstalledApp/InstalledApp.jsx
+++ src/Pages/InstalledApp/InstalledApp.jsx
@@ -4,33 +4,24 @@ import downloadImg from '../../assets/icon-downloads.png'
 
 const InstalledApp = ({b}) => {
   return (
-    <div>
-          <div className='p-8'>
-            <h1 className='text-center text-4xl font-bold'>Your Installed Apps</h1>
-            <p className='text-center text-gray-500 font-semibold mt-4'>Explore All Trending Apps on the Market developed by us</p>
-          </div>
-          <div className='flex justify-between items-center w-full mb-5'>
-            <h2 className='text-2xl font-bold '>1.Apps Found</h2>
-            <button className='btn btn-ghost'>Sort by Size</button>
-          </div>
-          <div className='flex justify-between items-center bg-white px-4 py-2 rounded-md shadow-md'>
+         <div className='flex justify-between items-center bg-white px-4 py-2 rounded-md shadow-md'>
             <div className='flex flex-row gap-3'>
               <div>
-              <img className='bg-gray-300 h-15 w-15 rounded-lg' src="" alt="" />
+              <img className='bg-gray-300 h-15 w-15 rounded-lg bg-cover' src={b.image} alt="" />
             </div>
             <div>
-              <h1 className='text-2xl font-bold mb-2'>Title Name</h1>
+              <h1 className='text-2xl font-bold mb-2'>{b.title}</h1>
               <div className='flex gap-4'>
                 <div className='flex gap-2'>
                   <img className='h-5 w-5' src={downloadImg} alt="" />
-                  <span>9m</span>
+                  <span>{b.downloads}</span>
                 </div>
                 <div className='flex gap-2'>
                   <img className='h-5 w-5' src={starImg} alt="" />
-                  <span>rating</span>
+                  <span>{b.ratingAvg}</span>
                 </div>
                 <div>
-                  <span>Size</span>
+                  <span>{b.size}MB</span>
                 </div>
               </div>
             </div>
@@ -39,7 +30,6 @@ const InstalledApp = ({b}) => {
             <button className='bg-[#00D390] rounded-md text-white font-semibold px-3 py-2'>Uninstall</button>
           </div>
           </div>
-        </div>
   )
 }
 
